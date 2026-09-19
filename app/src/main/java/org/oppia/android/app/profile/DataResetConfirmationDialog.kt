b35adc81844b794dbc737c0d84ff5d0accdcee07
Aryan Singh <aaryansssingh1938@gmail.com>
2026-09-19 15:24:57 +0530

    Reset Confirmation page updated

--- app/src/main/java/org/oppia/android/app/profile/DataResetConfirmationDialog.kt
+++ app/src/main/java/org/oppia/android/app/profile/DataResetConfirmationDialog.kt
@@ -1,10 +1,21 @@
 package org.oppia.android.app.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import org.oppia.android.app.ui.R
 
@@ -16,6 +27,9 @@ fun DataResetConfirmationDialog(
 ) {
   val appName = stringResource(R.string.app_name)
 
+  var inputText by remember { mutableStateOf("") }
+  val confirmationWord = "RESET"
+
   AlertDialog(
     title = {
       Text(
@@ -23,7 +37,17 @@ fun DataResetConfirmationDialog(
       )
     },
     text = {
+      Column{
       Text(stringResource(R.string.admin_confirm_app_wipe_message, appName))
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+          value = inputText,
+          onValueChange = { inputText = it },
+          label = { Text("Type '$confirmationWord' to confirm") },
+          singleLine = true,
+          modifier = Modifier.fillMaxWidth()
+        )
+      }
     },
     properties = DialogProperties(
       dismissOnClickOutside = false,
@@ -39,7 +63,8 @@ fun DataResetConfirmationDialog(
     },
     confirmButton = {
       TextButton(
-        onClick = { deleteAppData() }
+        onClick = { deleteAppData() },
+        enabled = inputText.equals(confirmationWord, ignoreCase = true)
       ) {
         Text(
           stringResource(
